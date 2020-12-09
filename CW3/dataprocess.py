43c65def13fb9975c3d5faed84ad046a1626f395
Eelin <41714138+892822247@users.noreply.github.com>
2020-12-09 13:32:35 +0800

    Update dataprocess.py

--- CW3/dataprocess.py
+++ CW3/dataprocess.py
@@ -7,14 +7,9 @@ dataset = pd.read_csv('Python\\CW3\\playerdata.csv')
 X = dataset.iloc[:,:-1].values
 y = dataset.iloc[:,-1].values
 
-# from boto import sns
-# sns.boxplot(x = dataset.ejection_fraction)
+# import seaborn as sns
+# sns.boxplot(x = dataset['BCK'])
 # plt.show()
-
-dataset.drop('TARGET', axis=1).corrwith(dataset.TARGET).plot(kind = 'bar', grid = True,
-                                                   figsize = (12, 8),
-                                                   title = "Correlation with Target")
-plt.show()
 
 df_1 = dataset[['GS','MIN','2FGM','2FGA','3FGM','3FGA','FTM','FTA','OREB',
                 'DREB','AST','STL','BCK','TO','PF','PTS']]
@@ -24,4 +19,9 @@ for item in df_1.columns:
     plt.boxplot(df_1[item],patch_artist = True, labels=[item])
     plt.ylabel('value')
 plt.tight_layout()
+plt.show()
+
+dataset.drop('TARGET', axis=1).corrwith(dataset.TARGET).plot(kind = 'bar', grid = True,
+                                                            figsize = (12, 8),
+                                                            title = "Correlation with Target")
 plt.show()
