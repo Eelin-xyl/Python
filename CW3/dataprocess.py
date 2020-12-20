b3f4e3fb72da30fae0fadb43075e1f66f9c5e561
Eelin <41714138+892822247@users.noreply.github.com>
2020-12-20 16:04:26 +0800

    Update dataprocess.py

--- CW3/dataprocess.py
+++ CW3/dataprocess.py
@@ -11,9 +11,9 @@ y = dataset.iloc[:,-1].values
 # sns.boxplot(x = dataset.ejection_fraction)
 # plt.show()
 
-dataset.drop(dataset[dataset['FTM'] > 20].index, inplace=True)
-dataset.drop(dataset[dataset['STL'] > 7.5].index, inplace=True)
-dataset.drop(dataset[dataset['TO'] > 6].index, inplace=True)
+# dataset.drop(dataset[dataset['FTM'] > 20].index, inplace=True)
+# dataset.drop(dataset[dataset['STL'] > 7.5].index, inplace=True)
+# dataset.drop(dataset[dataset['TO'] > 6].index, inplace=True)
 
 
 dataset.drop('TARGET', axis=1).corrwith(dataset.TARGET).plot(kind = 'bar', grid = True,
